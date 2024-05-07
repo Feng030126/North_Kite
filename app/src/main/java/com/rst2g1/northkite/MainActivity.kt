927401697f0dc6169915a866e26178d2116c7b62
jason <jasonyap0031@gmail.com>
2024-05-07 19:50:47 +0800

    Changed action bar color

--- app/src/main/java/com/rst2g1/northkite/MainActivity.kt
+++ app/src/main/java/com/rst2g1/northkite/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.rst2g1.northkite
 
+import android.content.Context
+import android.content.res.Resources
+import android.content.res.Resources.Theme
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Window
+import android.widget.ImageView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -31,5 +39,12 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        supportActionBar!!.setBackgroundDrawable(ResourcesCompat.getDrawable(resources, R.drawable.action_bar_bg, null))
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+
+        supportActionBar!!.setDisplayShowCustomEnabled(true)
+
+        supportActionBar!!.setCustomView(layoutInflater.inflate(R.layout.logo_layout, null))
     }
 }
